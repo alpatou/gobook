9e538d8c97c568c1204f1950edfc3876a8c28939
patal <patourasalexandros@gmail.com>
2023-07-01 01:41:18 +0300

    hello

---
+++ helloworld.go
@@ -0,0 +1,10 @@
+package main
+
+// package declaration , where it belongs
+// main is special , defines a stand alone program, not a library
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello,BF ")
+}
