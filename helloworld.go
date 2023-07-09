3c05fab24bbaef29a5c68e66a69d3ee9acf70188
l l <l@l.l>
2023-07-09 17:28:20 +0300

    ch1

--- helloworld.go
+++ helloworld.go
@@ -3,8 +3,29 @@ package main
 // package declaration , where it belongs
 // main is special , defines a stand alone program, not a library
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello,BF ")
+	s, sep := "", ""
+	for index, arg := range os.Args[0:] {
+		s += strconv.Itoa(index) + sep + arg + "\n"
+		sep = " "
+	}
+	fmt.Println(s)
+	smartArgumentPrinter()
+}
+
+func smartArgumentPrinter() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
+
+func smartLoop() {
+	for i, arg := range os.Args {
+		fmt.Printf("%d: %s\n", i, arg)
+	}
 }
